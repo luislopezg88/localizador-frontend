23cd80898f94ead8cb6a50b1d4803f0e4e85e4b1
Jorge C <jorgecolmenarez89@gmail.com>
2023-11-15 20:44:26 -0400

    cambiar base url

--- src/configs/constans.ts
+++ src/configs/constans.ts
@@ -1,1 +1,1 @@
-export const API_URL =  process.env.NODE_ENV === 'development' ? "http://localhost:4000/api" : "https://backend-evaluaci-n-de-salud-y-bienestar-para-rrhh-beryl.vercel.app/api";
+export const API_URL =  process.env.NODE_ENV === 'development' ? "http://localhost:4000/api" : "https://locallizador-backend.vercel.app/api";
